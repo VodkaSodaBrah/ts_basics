00b5b8de72f24633a5b4ba6ecf161be7e434814c
Michael Childress <mchildress@me.com>
2025-05-19 20:59:43 -0700

    Import iTransformer code and apply custom patches

---
+++ iTransformer/iTransformer/experiments/exp_basic.py
@@ -0,0 +1,45 @@
+import os
+import torch
+from model import Transformer, Informer, Reformer, Flowformer, Flashformer, \
+    iTransformer, iInformer, iReformer, iFlowformer, iFlashformer
+
+
+class Exp_Basic(object):
+    def __init__(self, args):
+        self.args = args
+        self.model_dict = {
+            'Transformer': Transformer,
+            'Informer': Informer,
+            'Reformer': Reformer,
+            'Flowformer': Flowformer,
+            'Flashformer': Flashformer,
+            'iTransformer': iTransformer,
+            'iInformer': iInformer,
+            'iReformer': iReformer,
+            'iFlowformer': iFlowformer,
+            'iFlashformer': iFlashformer,
+        }
+        self.device = self._acquire_device()
+        self.model = self._build_model().to(self.device)
+
+    def _build_model(self):
+        raise NotImplementedError
+        return None
+
+    def _acquire_device(self):
+        # Use the device selected in run.py (CUDA, MPS, or CPU)
+        device = self.args.device
+        print(f"Using device: {device}")
+        return device
+
+    def _get_data(self):
+        pass
+
+    def vali(self):
+        pass
+
+    def train(self):
+        pass
+
+    def test(self):
+        pass
